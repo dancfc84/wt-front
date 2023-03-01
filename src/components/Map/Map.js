1f3dbfa98d19e46aa3d906b1af675b4d6efc880d
dancfc84 <danwhittock2013@gmail.com>
2023-03-01 20:03:03 +0000

    Small changes index.js and Map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,15 +4,15 @@ import { useState, useMemo, useCallback, useRef } from "react";
 import {
   GoogleMap,
   Marker,
-  DirectionsRenderer,
-  MarkerClusterer,
 } from "@react-google-maps/api";
 
 
 export default function Map () {
 
+  //useMemo allows you to cache values between re-renders
   const testMark = useMemo(() => ({ lat: 51.2, lng: -0.29 }), []);
   const center = useMemo(() => ({ lat: 51.2, lng: -0.29 }), []);
+
 
   const mapRef = useRef();
 
@@ -21,37 +21,30 @@ export default function Map () {
     () => ({
       clickableIcons: false,
       disableDefaultUI: true,
-      //mapId = decides which map is shown, go to google maps platform to change and add more maps, 
       mapId: "a1fba3f6c23cfa4c",
     }),
     []
-
   );
 
-  const onLoad = useCallback((map) => (mapRef.current = map), []);
+  //allows you do cache a function definition betwene re-renders
+  const onLoad = useCallback((map) => (
+      mapRef.current = map), []);
+  
+  console.log(onLoad);
 
   return (
   <>
-  <div className={`${styles.main_div}`}>
-    <h1>Hello</h1>
-    <div>
-      <GoogleMap
-          zoom={10}
-          center={center}
-          options={options}
-          mapContainerClassName="map-container"
-          onLoad={onLoad}
-          onClick={ev => {
-            console.log("latitide = ", ev.latLng?.lat());
-            console.log("longitude = ", ev.latLng?.lng());
-            console.log(ev);
-            
-          }}
-        >
-          <Marker position={testMark}/>
-      </GoogleMap>
+    <div className={`${styles.main_div}`}>
+        <GoogleMap
+            zoom={10}
+            center={center}
+            options={options}
+            mapContainerClassName="map-container"
+            onLoad={onLoad}
+          >
+            <Marker position={testMark}/>
+        </GoogleMap>
     </div>
-  </div>
   </>
   )
 }
